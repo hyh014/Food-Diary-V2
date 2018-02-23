1d691258af4addab47b17ce2e1b33adf77d992f9
Hyunsoo Hong <hyh014@ucsd.edu>
2018-02-22 22:11:49 -0800

    asdf

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
-var io = require('socket.io')(http);
 
 var login = require('./routes/login');
 var index = require('./routes/index');
