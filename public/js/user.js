f7094258469192e7bb8cfc41e1cc6af067c054e1
Hyunsoo Hong <hyh014@ucsd.edu>
2018-03-01 19:56:55 -0800

    redesign entry

--- public/js/user.js
+++ public/js/user.js
@@ -76,3 +76,41 @@ $('#edit').click(function(){
 
   location.replace("/entry/"+sessionStorage.getItem('name'));
 });
+
+$('#diary').click(function(){
+  location.href="/diary/tester1";
+});
+$('#entry').click(function(){
+  location.href="/entry2/tester1";
+});
+$('#stat').click(function(){
+  location.href="/stat/tester1";
+});
+$('#setting').click(function(){
+  location.href="/setting/tester1";
+});
+
+$('#entry2rating').click(function(){
+
+  var food=document.getElementById('foodName').value;
+    sessionStorage.setItem('foodName',food);
+  var comment = document.getElementById('comments').value;
+  sessionStorage.setItem('comments',comment);
+});
+
+$('#addEntry2').click(function(){
+  sessionStorage.edit=false;
+  var date = $('#clock').text();
+  var time = date.substring(9);
+  var date = date.substring(0,8);
+  sessionStorage.setItem('name','tester1');
+  document.getElementById('taste').value= $('#tasteRate').rateit('value');
+  document.getElementById('health').value=$('#healthRate').rateit('value');
+  document.getElementById('mood').value=$('#moodRate').rateit('value');
+  document.getElementById('anxiety').value=$('#anxietyRate').rateit('value');
+  document.getElementById('date').value=date
+  document.getElementById('time').value=time;
+  document.getElementById('userid').value=sessionStorage.name;
+  document.getElementById('foodName').value = sessionStorage.foodName;
+  document.getElementById('comments').value = sessionStorage.comments;
+});
