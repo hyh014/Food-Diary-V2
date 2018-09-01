36e5c9f78dc5957612fd51355c8b435c38ff35a9
Hyunsoo Hong <hyh014@ucsd.edu>
2018-08-31 20:44:13 -0700

    Need to send data to Firebase

--- public/js/user.js
+++ public/js/user.js
@@ -1,15 +1,30 @@
-$('#diary').click(function(){
-  location.href="/diary/"+sessionStorage.name;
-});
-$('#entry').click(function(){
-  location.href="/entry2/"+sessionStorage.name;
-});
-$('#stat').click(function(){
-  location.href="/stat/"+sessionStorage.name;
-});
-$('#setting').click(function(){
-  location.href="/setting/"+sessionStorage.name;
-});
-$('#news').click(function(){
-  location.href="/news/"+sessionStorage.name;
-});
+const supported = 'mediaDevices' in navigator;
+let canvas = document.getElementById('canvas');
+let dataURL = document.getElementById('data');
+let img = new Image();
+
+function doSomethingWithFiles(fileList) {
+let file = null;
+for (let i = 0; i < fileList.length; i++) {
+  if (fileList[i].type.match(/^image\//)) {
+    file = fileList[i];
+    break;
+  }
+}
+if (file !== null) {
+ img.src = URL.createObjectURL(file);
+
+ console.log("Blob URL: "+ img.src);
+ dataURL.value = canvas.toDataURL('image/jpeg',1.0);
+ console.log("Data URL: "+ dataURL.value;
+ }
+
+}
+img.onload= function(){
+  canvas.getContext('2d').drawImage(img,0,0,img.width,    img.height
+                                       ,0,0,canvas.width, canvas.height);
+
+}
+
+const fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', (e) => doSomethingWithFiles(e.target.files));
--- routes/entry.js
+++ routes/entry.js
@@ -5,7 +5,7 @@ const firebase = require('firebase');
 // GET request to /login
 
 router.get('/', function(req, res, next) {
-      console.log(firebase.auth().currentUser.uid);
+      //console.log(firebase.auth().currentUser.uid);
 		res.render('entry');
 });
 
