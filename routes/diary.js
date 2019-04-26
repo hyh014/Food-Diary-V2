e6bd43387c0608c41f7f84c2d5164bcdee7eeed3
Hyunsoo Hong <hyh014@ucsd.edu>
2019-04-25 17:33:06 -0700

    working on css

--- routes/diary.js
+++ routes/diary.js
@@ -2,33 +2,38 @@
 var express = require('express');
 var router = express.Router();
 let firebase = require('firebase');
-
+require('dotenv').load();
 // GET request to /login
-
 router.get('/', function(req, res, next) {
-	let user = firebase.auth().currentUser;
-	if(user){
-		const UUID = firebase.auth().currentUser.uid;
-		let ref = firebase.database().ref('users/'+UUID);
+	res.render('diary');
+}
 
-		let list = {entry:[]};
-		ref.once('value',function(snapshot){
-			snapshot.forEach(function(child){
-				list.entry.unshift({
-					'key': child.key,
-					'data': child.val()
-				});
-			});
-			 res.render('diary',list);
-		});
-	}else{
-		res.render('login',{message:'Please Login First'});
-	}
-
-
-
-
-});
+//							UNCOMMENT ONCE DONE
+// router.get('/', function(req, res, next) {
+// 	let user = firebase.auth().currentUser;
+// 	if(user){
+// 		const UUID = firebase.auth().currentUser.uid;
+// 		let ref = firebase.database().ref('users/'+UUID);
+//
+// 		let list = {entry:[]};
+// 		ref.once('value',function(snapshot){
+// 			snapshot.forEach(function(child){
+// 				list.entry.unshift({
+// 					'key': child.key,
+// 					'data': child.val()
+// 				});
+// 			});
+// 			list.apiKey= process.env.APIKEY;
+// 			list.authDomain= process.env.AUTHDOMAIN;
+// 			list.databaseURL= process.env.DATABASEURL;
+// 			list.storageBucket= process.env.STORAGEBUCKET;
+// 			list.messagingSenderId= process.env.MESSAGINGSENDERID;
+// 			 res.render('diary',list);
+// 		});
+// 	}else{
+// 		res.render('login',{message:'Please Login First'});
+// 	}
+// });
 
 module.exports = router;
 /*
