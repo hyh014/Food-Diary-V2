83da331d12d0236219cf64abe1017fae7c0ba91e
Hyunsoo Hong <hyh014@ucsd.edu>
2019-09-09 14:23:23 -0700

    using modal instead

--- public/js/edit.js
+++ public/js/edit.js
@@ -7,8 +7,11 @@ $(document).ready(function(){
 		  databaseURL: result.databaseURL,
 		  storageBucket: result.storageBucket,
 		  messagingSenderId: result.messagingSenderId
-    }));
-    
+    });
+  }
+
+  });
+
   let list = document.getElementById('list');
   list.addEventListener('click',function(event){
     
@@ -30,48 +33,106 @@ $(document).ready(function(){
       {
         target = target.parentElement;
       }
-      cancel(target);
+      editForm(target);
     }
   },false);
   });
 
-  let cancel = (target) =>{
-    let textarea = $(target).siblings("diary-comment").children()[0] ;
-      textarea.readOnly = false;
-      $(textarea).attr('style','background-color:white');
-      let rating = $(target).siblings('.diary-rating')[0];
+  let editForm = (target) =>{
+    console.log($(target).parent()[0].dataset.key);
+    $('#editModal').show();
+  }
+// When the user clicks on <span> (x), close the modal
+$("span.close")[0].onclick = function() {
+  $('#editModal').css('display','none');
+}
 
-      let ratingArr = {
-        health:$(rating).children('text.health').text(),
-        taste:$(rating).children('text.taste').text(),
-        mood:$(rating).children('text.mood').text(),
-        anxiety:$(rating).children('text.anxiety').text()
-      }
-      $(rating).children('text.health').html("<input name='health' type='number' min='1' max='5' step='0.5'>");
-      $(rating).children('text.taste').html("<input name='taste' type='number' min='1' max='5' step='0.5'>");
-      $(rating).children('text.mood').html("<input name='mood' type='number' min='1' max='5' step='0.5'>");
-      $(rating).children('text.anxiety').html("<input name='anxiety' type='number' min='1' max='5' step='0.5'>");
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  var modal = document.getElementById("editModal");
+  if (event.target == modal) {
+    $('#editModal').css('display','none');
+  }
+}
 
-      $(target).hide();
-      $(target).siblings('.cancel').show();
-      $(target).siblings('.change').show();
+$("#modal-form-button").submit(function(e){
+
+});
+//   let cancel = (target) =>{
+//     let textarea = $(target).siblings("diary-comment").children()[0] ;
+//       textarea.readOnly = false;
+//       let previous = textarea.value;
+//       $(textarea).attr('style','background-color:white');
+//       let rating = $(target).siblings('.diary-rating')[0];
+
+//       let ratingArr = {
+//         health:$(rating).children('text.health').text(),
+//         taste:$(rating).children('text.taste').text(),
+//         mood:$(rating).children('text.mood').text(),
+//         anxiety:$(rating).children('text.anxiety').text()
+//       }
+//       $(rating).children('text.health').html("<input id='health' name='health' type='number' min='1' max='5' step='0.5'>");
+//       $(rating).children('text.taste').html("<input id='taste' name='taste' type='number' min='1' max='5' step='0.5'>");
+//       $(rating).children('text.mood').html("<input id='mood' name='mood' type='number' min='1' max='5' step='0.5'>");
+//       $(rating).children('text.anxiety').html("<input id='anxiety' name='anxiety' type='number' min='1' max='5' step='0.5'>");
+
+//       $(target).hide();
+//       $(target).siblings('.cancel').show();
+//       $(target).siblings('.change').show();
       
-    $(target).siblings('.cancel').on('click',function(){
-      $(rating).children('text.health').html(ratingArr.health);
-      $(rating).children('text.taste').html(ratingArr.taste);
-      $(rating).children('text.mood').html(ratingArr.mood);
-      $(rating).children('text.anxiety').html(ratingArr.anxiety);
+//     $(target).siblings('.cancel').on('click',function(){
+//       $(rating).children('text.health').html(ratingArr.health);
+//       $(rating).children('text.taste').html(ratingArr.taste);
+//       $(rating).children('text.mood').html(ratingArr.mood);
+//       $(rating).children('text.anxiety').html(ratingArr.anxiety);
 
-      $(target).siblings("diary-comment").children()[0].readOnly = true;
-      $(target).siblings("diary-comment").children().attr('style','background-color:transparent');
+//       $(target).siblings("diary-comment").children()[0].readOnly = true;
+//       $(target).siblings("diary-comment").children()[0].value = previous;
+//       $(target).siblings("diary-comment").children().attr('style','background-color:transparent');
 
-      $(target).siblings('.cancel').hide();
-      $(target).siblings('.change').hide();
+//       $(target).siblings('.cancel').hide();
+//       $(target).siblings('.change').hide();
 
-      $(target).show();
-  });
-  }
+//       $(target).show();
+//   });
+//   }
 
-  let change = (target) =>{
+//   let change = (target) =>{
+//     let textarea = $(target).siblings("diary-comment").children()[0];
+//     textarea.readOnly = false;
+//     $(textarea).attr('style','background-color:white');
+//       let rating = $(target).siblings('.diary-rating')[0];
 
-  }
+
+//       $(rating).children('text.health').html("<input id='health' name='health' type='number' min='1' max='5' step='0.5'>");
+//       $(rating).children('text.taste').html("<input id='taste' name='taste' type='number' min='1' max='5' step='0.5'>");
+//       $(rating).children('text.mood').html("<input id='mood' name='mood' type='number' min='1' max='5' step='0.5'>");
+//       $(rating).children('text.anxiety').html("<input id='anxiety' name='anxiety' type='number' min='1' max='5' step='0.5'>");
+
+//       $(target).hide();
+//       $(target).siblings('.cancel').show();
+//       $(target).siblings('.change').show();
+      
+//     $(target).siblings('.change').on('click',function(){
+//       let newRatingArr = {
+//         health:$('#health').val(),
+//         taste:$('#taste').val(),
+//         mood:$('#mood').val(),
+//         anxiety:$('#anxiety').val()
+//       }
+
+//       $(rating).children('text.health').html(newRatingArr.health + "&#9734;");
+//       $(rating).children('text.taste').html(newRatingArr.taste + "&#9734;");
+//       $(rating).children('text.mood').html(newRatingArr.mood + "&#9734;");
+//       $(rating).children('text.anxiety').html(newRatingArr.anxiety + "&#9734;");
+
+//       $(target).siblings("diary-comment").children()[0].readOnly = true;
+//       $(target).siblings("diary-comment").children().attr('style','background-color:transparent');
+
+//       $(target).siblings('.cancel').hide();
+//       $(target).siblings('.change').hide();
+
+//       $(target).show();
+//   });
+
+// }  
